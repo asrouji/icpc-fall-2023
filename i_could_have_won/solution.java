4a09a06fd7c5d8429a3b669900c75b8b9fe04fa4
Jgonz156 <Jgonz156@lion.lmu.edu>
2023-10-13 14:53:33 -0700

    fully cooked the java solution

--- i_could_have_won/solution.java
+++ i_could_have_won/solution.java
@@ -1,3 +1,5 @@
+package i_could_have_won;
+
 import java.util.*;
 
 class Solution {
@@ -18,11 +20,11 @@ class Solution {
                         bobPoints++;
                     }
 
-                    if (bobPoints > k) {
+                    if (bobPoints >= k) {
                         bobWins++;
                     }
 
-                    if (alicePoints > k) {
+                    if (alicePoints >= k) {
                         aliceWins++;
                     }
 
@@ -32,62 +34,15 @@ class Solution {
                     }
                 }
                 if (aliceWins > bobWins) {
-                    wins.add(aliceWins);
+                    wins.add(k);
                 }
             }
-
-            System.out.println(wins.get(0));
-            wins.forEach(System.out::print);
+            String output = "";
+            System.out.println(wins.size());
+            for (Integer k : wins) {
+                output += Integer.toString(k) + " ";
+            }
+            System.out.print(output.trim());
         }
     }
 }
-/*
- * import sys
- * 
- * games = sys.stdin.readlines()[0].strip()
- * 
- * winning_k = []
- * 
- * for k in range(1, len(games)):
- * alice_points = 0
- * bob_points = 0
- * 
- * alice_wins = 0
- * bob_wins = 0
- * 
- * for character in games:
- * # randomness
- * (alice_points, bob_points) =
- * if (character == "A"):
- * alice_points += 1
- * elif (character == "B"):
- * bob_points += 1
- * if (alice_won := alice_points >= k) or (bob_won := bob_points >= k):
- * alice_points = 0
- * bob_points = 0
- * if alice_won:
- * alice_wins += 1
- * elif bob_won:
- * bob_wins += 1
- * 
- * # actual solution
- * if (character == "A"):
- * alice_points += 1
- * elif (character == "B"):
- * bob_points += 1
- * if alice_points >= k:
- * alice_wins += 1
- * if bob_points >= k:
- * bob_wins += 1
- * if alice_points >= k or bob_points >= k:
- * alice_points = 0
- * bob_points = 0
- * 
- * if alice_wins > bob_wins:
- * winning_k.append(k)
- * 
- * print(len(winning_k))
- * 
- * for k in winning_k:
- * print(k, end=" ")
- */
