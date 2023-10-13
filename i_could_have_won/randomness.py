dab0385bdc3ebf35bb4862e37143f4b5c67b0695
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-10-13 14:19:56 -0700

    fixed style problems and made new style problems

--- i_could_have_won/randomness.py
+++ i_could_have_won/randomness.py
@@ -5,39 +5,13 @@ games = sys.stdin.readlines()[0].strip()
 winning_k = []
 
 for k in range(1, len(games)):
-    alice_points = 0
-    bob_points = 0
-
-    alice_wins = 0
-    bob_wins = 0
+    alice_points = bob_points = alice_wins = bob_wins = 0
 
     for character in games:
-        # randomness
-        # (alice_points, bob_points) =
-        if (character == "A"):
-            alice_points += 1
-        elif (character == "B"):
-            bob_points += 1
-        if (alice_won := alice_points >= k) or (bob_won := bob_points >= k):
-            alice_points = 0
-            bob_points = 0
-            if alice_won:
-                alice_wins += 1
-            elif bob_won:
-                bob_wins += 1
-
-        # actual solution
-        # if (character == "A"):
-        #     alice_points += 1
-        # elif (character == "B"):
-        #     bob_points += 1
-        # if alice_points >= k:
-        #     alice_wins += 1
-        # if bob_points >= k:
-        #     bob_wins += 1
-        # if alice_points >= k or bob_points >= k:
-        #     alice_points = 0
-        #     bob_points = 0
+        (alice_points, bob_points) = ((alice_points, bob_points + 1), (alice_points + 1, bob_points))[character == 'A']
+        if (alice_won := alice_points >= k) or bob_points >= k:
+            alice_points = bob_points = 0
+            (alice_wins, bob_wins) = ((alice_wins, bob_wins + 1), (alice_wins + 1, bob_wins))[alice_won]
 
     if alice_wins > bob_wins:
         winning_k.append(k)
