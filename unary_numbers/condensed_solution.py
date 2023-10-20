e2f7a8553d2f15adf022e3d7e60395796dfb11fa
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-10-20 14:01:46 -0700

    one line solution

--- unary_numbers/condensed_solution.py
+++ unary_numbers/condensed_solution.py
@@ -1,10 +1,1 @@
-
-# print(*[f"{line.strip()} {''.join([*['1' for _ in range(1, int(line))], '0'])}\n" for line in sys.stdin.readlines()], end='')
-
-import sys
-
-for line in sys.stdin.readlines():
-    print(
-        f"{line.strip()} {'1' * (int(line) - 1)}0")
-
-exit()
+import sys; print(*[f'{line.strip()} {"1" * (int(line) - 1)}0' for line in sys.stdin.readlines()], sep='\n'); exit()
