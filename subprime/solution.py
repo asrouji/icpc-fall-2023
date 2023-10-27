5520025fe36ba423b9e5620671e887fd5844c2ea
asrouji <asrouji@lion.lmu.edu>
2023-10-27 13:42:42 -0700

    remove debug print

--- subprime/solution.py
+++ subprime/solution.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def generate_primes(limit):
     sieve = [True] * (limit + 1)
     sieve[0] = sieve[1] = False
@@ -14,21 +15,19 @@ def generate_primes(limit):
             primes.append(p)
     return primes
 
+
 def count_primes_with_substring(primes, i, j, substring):
     count = 0
-    for prime in primes[i-1:j]:
+    for prime in primes[i - 1:j]:
         if substring in str(prime):
             count += 1
     return count
+
 
 primes_limit = 10000000
 all_primes = generate_primes(primes_limit)
 
 for input in sys.stdin.readlines():
     [i, j, sub] = input.strip().split(" ")
-    print(i, j, sub)
     total = count_primes_with_substring(all_primes, int(i), int(j), sub)
     print(total)
-
-
-
